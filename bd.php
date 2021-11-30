b44fc5038a3b80bfeabb2bab9f8de137b46b4998
BastianHi <93065521+BastianHi@users.noreply.github.com>
2021-11-30 14:05:47 -0300

    Update bd.php

--- bd.php
+++ bd.php
@@ -11,4 +11,11 @@ $db_name="7soft2021";
 //string conexion
 $con = mysqli_connect($db_host, $db_user,$db_pass, $db_name);
 
+if(mysqli_connect_errno())
+{
+   echo 'No se pudo conectar a la base de datos: ' .mysqli_connect_error();
+}else
+{
+    echo 'Se conecto';
+}
 ?>
