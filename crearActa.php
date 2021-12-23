5341e38e6c55ba1b5c21f1e70445c9f8bc9b0c73
BastianHi <93065521+BastianHi@users.noreply.github.com>
2021-12-23 08:49:49 -0300

    Update crearActa.php

--- crearActa.php
+++ crearActa.php
@@ -1,13 +1,14 @@
 <?php
 require_once("bd.php");
-$id_acta='1250';
-
+  
 
 if (isset($_POST["coment1"])) {
     $descripcion = $_POST["coment1"];
 }
+  $id_reu = $_POST["id_r"];
 
-$crearActaSQL = "insert into `acta` (`id_acta`, `acta`) values (".$id_acta.",'$descripcion')";
+
+$crearActaSQL = "insert into `acta` (`acta`, `id_reu`) values ('$descripcion',".$id_reu.")";
 $crearActa = mysqli_query($con, $crearActaSQL);
-header("Location: acta.php");
+header("Location: index.php");
 ?>
